91ac03c95e8ad588768d41040cf90a8a96061f0d
박세준 <sjpark@selab.co.kr>
2022-11-17 10:44:16 +0900

    ThreadLocal를 적용한 LogTrace를 프로젝트에 적용

---
+++ src/main/java/spring/advanced/LogTraceConfig.java
@@ -0,0 +1,15 @@
+package spring.advanced;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import spring.advanced.trace.logtrace.LogTrace;
+import spring.advanced.trace.logtrace.ThreadLocalLogTrace;
+
+@Configuration
+public class LogTraceConfig {
+
+    @Bean
+    public LogTrace logTrace() {
+        return new ThreadLocalLogTrace();
+    }
+}
---
+++ src/main/java/spring/advanced/app/v3/OrderServiceV3.java
@@ -0,0 +1,29 @@
+package spring.advanced.app.v3;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import spring.advanced.trace.TraceId;
+import spring.advanced.trace.TraceStatus;
+import spring.advanced.trace.hellotrace.HelloTraceV2;
+import spring.advanced.trace.logtrace.LogTrace;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV3 {
+
+    private final OrderRepositoryV3 orderRepository;
+    private final LogTrace trace;
+
+    public void orderItem(String orderId) {
+
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderService.orderItem()");
+            orderRepository.save(orderId);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+}
