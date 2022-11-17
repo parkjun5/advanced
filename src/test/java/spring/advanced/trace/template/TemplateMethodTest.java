76ea2236be5770fccd23cdbb4d73af5e83bfc98c
박세준 <sjpark@selab.co.kr>
2022-11-17 11:19:20 +0900

    AbstractTemplate 패턴을 사용한 중복 메서드 해결방안
     - 변하지 않는 코드와 변하는 코드를 분리하여 Abstarct로 나눈다.
     - 메서드를 상속해서 해결
     - 변화가 있을 때 일괄 변경 가능 -> 단일체계원칙을 지킬 수 있다.

---
+++ src/test/java/spring/advanced/trace/template/TemplateMethodTest.java
@@ -0,0 +1,55 @@
+package spring.advanced.trace.template;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import spring.advanced.trace.template.code.AbstractTemplate;
+import spring.advanced.trace.template.code.SubClassLogic1;
+import spring.advanced.trace.template.code.SubClassLogic2;
+
+@Slf4j
+public class TemplateMethodTest {
+
+    @Test
+    void templateMethodV0() {
+        logic1();
+        logic2();
+    }
+
+    private void logic1() {
+        long startTime = System.currentTimeMillis();
+        // 비지니스 로직 실행
+        log.info("비지니스 로직1 실행");
+        // 비지니스 로직 종료
+        long endTime = System.currentTimeMillis();
+
+        long resultTime = endTime - startTime;
+        log.info("resultTIme={}", resultTime);
+    }
+
+    private void logic2() {
+        long startTime = System.currentTimeMillis();
+        // 비지니스 로직 실행
+        log.info("비지니스 로직2 실행");
+        // 비지니스 로직 종료
+        long endTime = System.currentTimeMillis();
+
+        long resultTime = endTime - startTime;
+        log.info("resultTIme={}", resultTime);
+    }
+
+    /**
+     * 템플릿 메서드 패턴 적용
+     */
+    @Test
+    void templateMethodV1() {
+        AbstractTemplate logic1 = new SubClassLogic1();
+        logic1.execute();
+
+        AbstractTemplate logic2 = new SubClassLogic2();
+        logic2.execute();
+    }
+
+    private void templateLogic1() {
+
+    }
+}
